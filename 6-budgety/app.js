11a68f5b773d14f8dde94f77b6a62bc0ac06d5c7
armitage <=>
2020-02-16 23:26:02 +0100

    Add clearing the input fields.

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -98,6 +98,20 @@ var UIController = (function(){
             // Insert the HTML into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
+        
+        clearFields: function() {
+            var fields, fieldsArr;
+
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue); // Returns a list
+
+            fieldsArr = Array.prototype.slice.call(fields); // Set the this variable of the function to fields, because a list does not have the slice method.
+
+            fieldsArr.forEach(function(current, index, array) {
+                current.value = "";
+            });
+
+            fieldsArr[0].focus();
+        },
 
         getDOMstrings: function() {
             return DOMstrings;
@@ -133,9 +147,12 @@ var controller = (function(budgetCtrl, UICtrl){
         // 3. Add the item to the UI
         UICtrl.addListItem(newItem, input.type);
 
-        // 4. Calculate the budget
+        // 4. Clear the fields
+        UICtrl.clearFields();
 
-        // 5. Display the budget on the UI
+        // 5. Calculate the budget
+
+        // 6. Display the budget on the UI
     }
 
     return {
