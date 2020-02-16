0efbf0fb8938f406f4c101b99f84f7e95ef726e1
armitage <=>
2020-02-16 19:32:41 +0100

    Add an initalization function.

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -29,13 +29,24 @@ var UIController = (function(){
 
 // Global app controller
 var controller = (function(budgetCtrl, UICtrl){
-    var DOM = UICtrl.getDOMstrings();
+
+    var setupEventListeners = function() {
+        var DOM = UICtrl.getDOMstrings();
+
+        document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+
+        document.addEventListener('keypress', function(event) {
+            // If enter was pressed
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+        }); 
+    }
 
     var ctrlAddItem = function() {
         // 1. Get the field input data
         var input = UICtrl.getInput();
-        console.log(input);
-
+        
         // 2. Add the item to the budget controller
 
         // 3. Add the item to the UI
@@ -45,14 +56,12 @@ var controller = (function(budgetCtrl, UICtrl){
         // 5. Display the budget on the UI
     }
 
-    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
-
-    document.addEventListener('keypress', function(event) {
-        // If enter was pressed
-        if (event.keyCode === 13 || event.which === 13) {
-            ctrlAddItem();
+    return {
+        init: function() {
+            console.log('Application has started.');
+            setupEventListeners();
         }
-    }); 
-
-
+    }
 })(budgetController, UIController);
+
+controller.init();
