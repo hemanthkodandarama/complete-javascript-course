0102d61e8ccc4e54af2ca56182adf77fd9094d47
acneuromancer <dokukodo108@gmail.com>
2020-02-16 18:50:48 +0100

    Add reading input data.

--- 6-budgety/app.js
+++ 6-budgety/app.js
@@ -1,15 +1,40 @@
+// Budget controller
 var budgetController = (function() {
     
 })();
 
+// UI Controller
 var UIController = (function(){
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
 
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
 })();
 
+// Global app controller
 var controller = (function(budgetCtrl, UICtrl){
+    var DOM = UICtrl.getDOMstrings();
 
     var ctrlAddItem = function() {
         // 1. Get the field input data
+        var input = UICtrl.getInput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -18,15 +43,14 @@ var controller = (function(budgetCtrl, UICtrl){
         // 4. Calculate the budget
 
         // 5. Display the budget on the UI
-
-        console.log('It works!');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
+        // If enter was pressed
         if (event.keyCode === 13 || event.which === 13) {
-            console.log('Enter was pressed.');
+            ctrlAddItem();
         }
     }); 
 
