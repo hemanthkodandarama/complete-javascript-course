f110e915bd64760e7f1a94b498a31ee73510feb0
acneuromancer <dokukodo108@gmail.com>
2020-03-24 23:10:17 +0100

    Add Building the Search View - Part 1

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,4 +1,6 @@
 import Search from './models/Search';
+import * as searchView from './views/searchView'; 
+import { elements } from './views/base';
 
 /**
  * Global state of the app
@@ -12,37 +14,29 @@ const state = {};
 
 const controlSearch = async () => {
   // 1) Get query from the view
-  const query = 'pizza'; // TODO
+  const query = searchView.getInput();
+  console.log(query);
+  
 
   if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
 
     // 3) Prepare UI for results
-
+    searchView.clearInput();
+    searchView.clearResults();
+    
     // 4) Search for recipes
     await state.search.getResults();
 
     // 5) Render results on UI
+    searchView.renderResults(state.search.result);
     console.log(state.search.result);
+    
   }
-  
-
-
-
-
 };
 
-document.querySelector('.search').addEventListener('submit', e => {
+elements.searchForm.addEventListener('submit', e => {
   e.preventDefault();
   controlSearch();
 });
-
-
-
-
-
-const search = new Search('pizza');
-console.log(search);
-
-search.getResults();
