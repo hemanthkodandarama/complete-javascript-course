7bd46f2647315c26c9f2d3f76301cfc53345e71d
acneuromancer <dokukodo108@gmail.com>
2020-05-19 01:16:29 +0200

    Add building the shopping list controller

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -2,7 +2,8 @@ import Search from './models/Search';
 import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
-import * as recipeView from './views/recipeView'; 
+import * as recipeView from './views/recipeView';
+import * as listView from './views/listView'; 
 import { elements, renderLoader, clearLoader } from './views/base';
 
 /**
@@ -14,7 +15,12 @@ import { elements, renderLoader, clearLoader } from './views/base';
  */
 
 const state = {};
+// For testing
+window.state = state;
 
+/**
+ * Search controller
+ */
 const controlSearch = async () => {
   // 1) Get query from the view
   const query = searchView.getInput();
@@ -61,7 +67,6 @@ elements.searchResPages.addEventListener('click', e => {
 /**
  * Recipe controller
  */
-
 const controlRecipe = async () => {
   const id = window.location.hash.replace('#', '');
   console.log(id);
@@ -101,6 +106,39 @@ const controlRecipe = async () => {
 // window.addEventListener('load', controlRecipe);
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+/** 
+ * List controller
+ */
+const controlList = () => {
+  // Create a new list if there is none yet
+  if (!state.list) {
+    state.list = new List();
+  }
+  // Add each ingredient to the list and UI
+  state.recipe.ingredients.forEach(el => {
+    const item = state.list.addItem(el.count, el.unit, el.ingredient);
+    listView.renderItem(item);
+  });
+}
+
+// Handle delete and update list item events
+elements.shopping.addEventListener('click', e => {
+  const id = e.target.closest('.shopping__item').dataset.itemid;
+
+  // Handle the delete button
+  if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+    // Delete from state
+    state.list.deleteItem(id);
+   
+    // Delete from UI
+    listView.deleteItem(id);
+  } else if (e.target.matches('.shopping__count-value')) {
+    // Handle the count update
+    const val = parseFloat(e.target.value, 10);
+    state.list.updateCount(id, val);
+  }
+});
+
 // Handling recipe button clicks
 elements.recipe.addEventListener('click', e => {
   if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -113,6 +151,8 @@ elements.recipe.addEventListener('click', e => {
     // Increase button is clicked
     state.recipe.updateServings('inc');
     recipeView.updateServingsIngredients(state.recipe);
+  } else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+    controlList();
   }
 });
 
--- 9-forkify/src/js/views/listView.js
+++ 9-forkify/src/js/views/listView.js
@@ -19,7 +19,9 @@ export const renderItem = item => {
   elements.shopping.insertAdjacentHTML('beforeend', markup);
 };
 
-export const deleteItem = item => {
-  const item = document.querySelector(`[date-itemid="${id}"]`);
-  item.parentElement.removeChild(item);
+export const deleteItem = id => {
+  const item = document.querySelector(`[data-itemid="${id}"]`);
+  if (item) {
+    item.parentElement.removeChild(item);
+  }
 };
